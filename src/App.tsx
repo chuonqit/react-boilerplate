431769dbcf52510106a4d4da18a8b3d096f58a23
CHUONQIT <chuonqit@gmail.com>
2022-05-21 23:42:40 +0700

    add demo image url

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,35 @@ function App() {
   const [image, setImage] = useState<string>("https://i.imgur.com/s7gBWif.jpg");
 
   return (
-    <div className="App" style={{ padding: 16 }}>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 16,
+        flexDirection: "column",
+        padding: 16,
+      }}
+    >
       <Photo360 src={image} />
-      <Photo360 src={image} />
-      <br />
       <input
         type="text"
         value={image}
         onChange={(e) => setImage(e.target.value)}
         style={{ width: 500 }}
       />
+      <br />
+      <ol>
+        <li>https://i.imgur.com/QvlEsl7h.jpg</li>
+        <li>
+          https://thumbs.dreamstime.com/b/equirectangular-panorama-360-degrees-21184868.jpg
+        </li>
+        <li>http://i.imgur.com/EcT6CCv.jpg</li>
+        <li>
+          https://gc.360-data.com/tours/Ht50Vngjb5XQ/Ht50Vngjb5XQ-lQS7zie67nFJ-thumb.jpg
+        </li>
+      </ol>
     </div>
   );
 }
