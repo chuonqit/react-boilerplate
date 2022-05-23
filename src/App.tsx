7b25985500a8aa6c192d94a5aefa9c24106f2974
CHUONQIT <chuonqit@gmail.com>
2022-05-23 08:44:31 +0700

    custom loader, error msg, change theme radar

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,17 @@ function App() {
 
   return (
     <div className="App">
-      <Panorama src="https://i.imgur.com/Jvqqmx9.jpg" width="100vw" height="100vh" />
+      <Panorama
+        src="https://swall.teahub.io/photos/small/5-54070_new-york-ultra-wide.jpg"
+        width="500px"
+        height="250px"
+      />
+      <br />
+      <Panorama
+        src="https://thumbs.dreamstime.com/b/archipelago-sea-bay-hdri-environment-map-round-panorama-spherical-equidistant-projection-high-resolution-n-d-rendering-224018392.jpg"
+        width="500px"
+        height="250px"
+      />
     </div>
   );
 }
--- src/components/Panorama/index.tsx
+++ src/components/Panorama/index.tsx
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { useState, Suspense, useEffect, WheelEvent } from "react";
-import { Canvas, useThree, useLoader, useFrame, Euler } from "@react-three/fiber";
+import { Canvas, useThree, useLoader, useFrame } from "@react-three/fiber";
 import { Html, OrbitControls, Loader, useProgress } from "@react-three/drei";
 import { AiOutlineExpand, AiOutlineCompress, AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
@@ -14,7 +14,7 @@ type Props = {
 
 const Panorama = ({ src, width = "600px", height = "300px" }: Props) => {
   const [fov, setFov] = useState<number>(80);
-  const [rotation, setRotation] = useState<number>(0);
+  const [radarPosition, setRadarPosition] = useState<number>(0);
   const [isFullScreen, setIsFullScreen] = useState<boolean>(false);
   const [progressActive, errors] = useProgress((state) => {
     return [state.progress, state.errors];
@@ -35,23 +35,28 @@ const Panorama = ({ src, width = "600px", height = "300px" }: Props) => {
 
   const Environment = () => {
     const { scene } = useThree();
-    const texture = useLoader(THREE.TextureLoader, src);
-    texture.mapping = THREE.EquirectangularReflectionMapping;
-    scene.background = texture;
+    try {
+      const texture = useLoader(THREE.TextureLoader, src);
+      texture.mapping = THREE.EquirectangularReflectionMapping;
+      scene.background = texture;
 
-    const vector = new THREE.Vector3();
-    const center = new THREE.Vector3();
-    const spherical = new THREE.Spherical();
+      const vector = new THREE.Vector3();
+      const center = new THREE.Vector3();
+      const spherical = new THREE.Spherical();
 
-    useFrame((state: any) => {
-      vector.copy(state.camera.position).sub(center);
-      spherical.setFromVector3(vector);
-      const sphericalRotation = spherical.theta;
-      setRotation(sphericalRotation);
-
-      state.camera.fov = fov;
-      state.camera.updateProjectionMatrix();
-    });
+      useFrame((state: any) => {
+        vector.copy(state.camera.position).sub(center);
+        spherical.setFromVector3(vector);
+        const thetaPosition = spherical.theta;
+        setRadarPosition(thetaPosition);
+        state.camera.fov = fov;
+        state.camera.updateProjectionMatrix();
+      });
+    } catch (error) {
+      if (typeof error === "string") {
+        console.log(error);
+      }
+    }
 
     return null;
   };
@@ -70,7 +75,7 @@ const Panorama = ({ src, width = "600px", height = "300px" }: Props) => {
     }
   };
 
-  return !errors.includes(src) ? (
+  return (
     <div
       className={isFullScreen ? "panorama-wrapper fullscreen" : "panorama-wrapper"}
       style={{
@@ -78,65 +83,85 @@ const Panorama = ({ src, width = "600px", height = "300px" }: Props) => {
         height: height,
       }}
     >
-      <Canvas
-        flat={true}
-        linear={true}
-        onWheel={onMouseWheel}
-        camera={{ position: [0, 0, 2.75], fov: fov }}
-      >
-        <Suspense
-          fallback={
-            <Html>
-              <Loader initialState={() => true} />
-            </Html>
-          }
-        >
-          <OrbitControls
-            enableDamping
-            enableZoom={false}
-            enablePan={false}
-            dampingFactor={0.03}
-            rotateSpeed={-0.3}
-          />
-          <Environment />
-        </Suspense>
-      </Canvas>
-      {progressActive === 100 && (
+      {!errors.includes(src) ? (
         <>
-          <div className="panorama-control">
-            <div className="panorama-control--wrapper">
-              <button className="panorama-control--btn" onClick={handleFullScreen}>
-                {isFullScreen ? (
-                  <AiOutlineCompress className="panorama-control--btn__icon" />
-                ) : (
-                  <AiOutlineExpand className="panorama-control--btn__icon" />
-                )}
-              </button>
-              <button
-                className="panorama-control--btn"
-                onClick={() => setFov(fov <= 10 ? 10 : fov - 7)}
-                disabled={fov <= 10}
-              >
-                <AiOutlinePlus className="panorama-control--btn__icon" />
-              </button>
-              <button
-                className="panorama-control--btn"
-                onClick={() => setFov(fov >= 80 ? 80 : fov + 7)}
-                disabled={fov >= 80}
-              >
-                <AiOutlineMinus className="panorama-control--btn__icon" />
-              </button>
+          <Canvas
+            flat={true}
+            linear={true}
+            onWheel={onMouseWheel}
+            camera={{ position: [-2, 0.5, 3], fov: fov }}
+          >
+            <Suspense fallback={null}>
+              <OrbitControls
+                enableDamping
+                enableZoom={false}
+                enablePan={false}
+                dampingFactor={0.03}
+                rotateSpeed={-0.3}
+              />
+              <Environment />
+            </Suspense>
+          </Canvas>
+
+          {progressActive === 100 && (
+            <>
+              <div className="panorama-control">
+                <div className="panorama-control--wrapper">
+                  <button className="panorama-control--btn" onClick={handleFullScreen}>
+                    {isFullScreen ? (
+                      <AiOutlineCompress className="panorama-control--btn__icon" />
+                    ) : (
+                      <AiOutlineExpand className="panorama-control--btn__icon" />
+                    )}
+                  </button>
+                  <button
+                    className="panorama-control--btn"
+                    onClick={() => setFov(fov <= 10 ? 10 : fov - 7)}
+                    disabled={fov <= 10}
+                  >
+                    <AiOutlinePlus className="panorama-control--btn__icon" />
+                  </button>
+                  <button
+                    className="panorama-control--btn"
+                    onClick={() => setFov(fov >= 80 ? 80 : fov + 7)}
+                    disabled={fov >= 80}
+                  >
+                    <AiOutlineMinus className="panorama-control--btn__icon" />
+                  </button>
+                </div>
+              </div>
+              <div className="panorama-radar" style={{ transform: `rotate(${radarPosition}rad)` }}>
+                <i></i>
+                <i></i>
+              </div>
+            </>
+          )}
+        </>
+      ) : (
+        progressActive === 100 && (
+          <div className="panorama-error">
+            <div className="panorama-error--text">
+              <strong>Không thể tải hình ảnh:</strong>
+              <a href={src} target="_blank">
+                {src}
+              </a>
             </div>
           </div>
-          <div className="panorama-radar" style={{ transform: `rotate(${rotation}rad)` }}>
-            <i></i>
-            <i></i>
+        )
+      )}
+      {progressActive < 100 && (
+        <div className="panorama-loader">
+          <div className="panorama-loader--wrapper">
+            <div className="panorama-loader--main"></div>
+            <div
+              className="panorama-loader--progress"
+              style={{ width: `${progressActive}%` }}
+            ></div>
           </div>
-          {/* <div className="panorama-scan" style={{ transform: `rotate(${rotation}rad)` }}></div> */}
-        </>
+        </div>
       )}
     </div>
-  ) : null;
+  );
 };
 
 export default Panorama;
